737846b7e67bb12118ccd05086f5ecce3cea0e98
Amir <amir@impssbl.com>
2024-03-09 14:26:40 +0100

    First working version

---
+++ workshop/hackathon_code/main.py
@@ -0,0 +1,125 @@
+# Hello!
+# I'm gonna build a chatbot with memory using RAG, for customers to ask their questions about the products and services of a company.
+
+# Step 1: Integrate the data
+# - We have to build vector embeddings for the data
+
+# Step 2: Get the query and find the related document
+
+# Step 3: Answer the question according to the document
+
+# Step 4: If the answer is not in the document, ask the user to ask to the customer service
+
+## Step 1
+
+import os
+
+from dotenv import load_dotenv
+from langchain.memory import ConversationBufferWindowMemory
+from langchain.llms.bedrock import Bedrock
+from langchain.chains import ConversationalRetrievalChain
+
+from langchain.embeddings import BedrockEmbeddings
+from langchain.indexes import VectorstoreIndexCreator
+from langchain.vectorstores import FAISS
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.document_loaders import PyPDFLoader, UnstructuredFileLoader
+
+from prompts import QA_CHAIN_PROMPT
+
+# import langsmith to debug easier
+import langchain
+_ = load_dotenv("credentials.env")
+
+try:  # for local debug
+    os.environ["LANGCHAIN_TRACING_V2"] =os.getenv("LANGCHAIN_TRACING_V2")
+    os.environ["LANGCHAIN_ENDPOINT"] = os.getenv("LANGCHAIN_ENDPOINT")
+    os.environ["LANGCHAIN_API_KEY"] = os.getenv("LANGCHAIN_API_KEY")
+    os.environ["LANGCHAIN_PROJECT"] = os.getenv("LANGCHAIN_PROJECT")
+except:
+    pass
+
+
+
+def get_llm(prompt):
+    model_kwargs = {  # AI21
+        "maxTokens": 1024,
+        "temperature": 0,
+        "topP": 0.3,
+        "stopSequences": ["Human:"],
+        "countPenalty": {"scale": 0},
+        "presencePenalty": {"scale": 0},
+        "frequencyPenalty": {"scale": 0},
+        "prompt": prompt
+    }
+
+    llm = Bedrock(
+        credentials_profile_name="default",
+        # sets the profile name to use for AWS credentials (if not the default)
+        region_name="us-east-1",  # sets the region name (if not the default)
+        model_id="ai21.j2-ultra-v1",  # set the foundation model
+        model_kwargs=model_kwargs)  # configure the properties for Claude
+
+    return llm
+
+
+def get_index():  # creates and returns an in-memory vector store to be used in the application
+
+    embeddings = BedrockEmbeddings(
+        credentials_profile_name="default",
+        # sets the profile name to use for AWS credentials (if not the default)
+        region_name="us-east-1",  # sets the region name (if not the default)
+    )  # create a Titan Embeddings client
+
+    pdfs_path = "docs"  # assumes local PDF file with this name
+
+    for pdf_path in os.listdir(pdfs_path):
+        loader = UnstructuredFileLoader(file_path=pdf_path, mode="paged")
+
+    text_splitter = RecursiveCharacterTextSplitter(  # create a text splitter
+        separators=["\n\n", "\n", ".", " "],
+        # split chunks at (1) paragraph, (2) line, (3) sentence, or (4) word, in that order
+        chunk_size=1000,  # divide into 1000-character chunks using the separators above
+        chunk_overlap=100  # number of characters that can overlap with previous chunk
+    )
+
+    index_creator = VectorstoreIndexCreator(  # create a vector store factory
+        vectorstore_cls=FAISS,  # use an in-memory vector store for demo purposes
+        embedding=embeddings,  # use Titan embeddings
+        text_splitter=text_splitter,  # use the recursive text splitter
+    )
+
+    index_from_loader = index_creator.from_loaders([loader])  # create an vector store index from the loaded PDF
+
+    return index_from_loader  # return the index to be cached by the client app
+
+
+def get_memory():  # create memory for this chat session
+
+    memory = ConversationBufferWindowMemory(memory_key="chat_history",
+                                            return_messages=True)  # Maintains a history of previous messages
+
+    return memory
+
+
+def get_rag_chat_response(input_text, memory, index, prompt):  # chat client function
+
+    llm = get_llm(prompt=prompt)
+
+    conversation_with_retrieval = ConversationalRetrievalChain.from_llm(llm, index.vectorstore.as_retriever(),
+                                                                        memory=memory)
+
+    chat_response = conversation_with_retrieval(
+        {"question": input_text})  # pass the user message and summary to the model
+
+    return chat_response['answer']
+
+
+input_text = "What is the power source of this oven?"
+
+response = get_rag_chat_response(input_text=input_text,
+                                 memory=get_memory(),
+                                 index=get_index(),
+                                 prompt=QA_CHAIN_PROMPT)
+
+print(response)
---
+++ workshop/hackathon_code/prompts.py
@@ -0,0 +1,26 @@
+from langchain.prompts import PromptTemplate
+
+
+qa_template = """You have been provided with a question, delimited by '#####'. The following pieces of context, delimited by '-----', will assist you in answering the question. The source file for each piece of context is provided at its beginning and is delimited by '<<<' and '>>>'.
+Your tasks are to:
+1- Provide a helpful answer to the question using ONLY the information provided in the context.
+2- List the source file or files that you utilized to answer the question. If you cannot answer the question with the provided context, return an empty array.
+3- Respond a JSON object formatted according to the following schema:
+
+```JSON
+{{
+    "answer": "string", // Answer to the question using the provided context
+    "source": ["string"], // Array containing strings, each representing a source file used to craft the answer
+}}
+```
+#####{question}#####
+
+-----{context}-----
+
+REMEMBER, if you don't know the answer, state that you don't know. Do not attempt to fabricate an answer.
+REMEMBER, if you don't know the answer, the source should be an empty array.
+REMEMBER, .Ensure that you only output the JSON, without any additional notes or explanations.
+Ensure that the answer within the json is concise and informative, offering sufficient detail without being overly terse or telegraphic.
+"""
+
+QA_CHAIN_PROMPT = PromptTemplate.from_template(qa_template)
